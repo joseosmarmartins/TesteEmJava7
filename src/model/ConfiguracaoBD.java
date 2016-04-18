33df59bbc7d185ba4239ecc4ac4d17cc388c1f90
José Osmar Martins Silva <joseosmarmartins@hotmail.com>
2016-04-17 23:20:53 -0300

    no changes

--- src/model/ConfiguracaoBD.java
+++ src/model/ConfiguracaoBD.java
@@ -4,7 +4,7 @@ public class ConfiguracaoBD {
 
     public static final String url = "jdbc:postgresql://localhost/testeEmJava7";
     public static final String usuario = "postgres";
-    public static final String senha = "bullshit06";
+    public static final String senha = "testeEmJava7";
 
     public static final String driver = "org.postgresql.Driver";
 }
